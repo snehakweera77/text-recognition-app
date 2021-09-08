56c3195a56c24b456f2ccbe6203cbfcebfb14f56
snehakweera77 <snehakweera77@gmail.com>
2021-09-08 22:23:24 +0530

    capture button onClick & scanner activity

--- app/src/main/java/com/example/textrecognition/MainActivity.kt
+++ app/src/main/java/com/example/textrecognition/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.textrecognition
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var captureBtn : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        captureBtn.setOnClickListener {
+            var intent = Intent(this, ScannerActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
